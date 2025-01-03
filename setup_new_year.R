fba6bd2db7c89b87ef31cddc538419e493104595
JaneSullivan-NOAA <jane.sullivan@noaa.gov>
2025-01-03 10:26:32 -0900

    assessment files and readme

---
+++ setup_new_year.R
@@ -0,0 +1,8 @@
+# Assessment year
+YEAR <- 2024
+dir.create(file.path(YEAR))
+dir.create(file.path(YEAR, 'data'))
+dir.create(file.path(YEAR, 'data', 'raw'))
+dir.create(file.path(YEAR, 'R'))
+dir.create(file.path(YEAR, 'safe'))
+dir.create(file.path(YEAR, 'slides'))
